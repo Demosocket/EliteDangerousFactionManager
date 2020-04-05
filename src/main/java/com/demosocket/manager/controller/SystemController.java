846cdb2fbb95eb9f17aa624ebc4b4cfd553453f9
demosocket <demosocket.@gmail.com>
2020-04-05 15:22:23 +0300

    added header and home page

--- src/main/java/com/demosocket/manager/controller/SystemController.java
+++ src/main/java/com/demosocket/manager/controller/SystemController.java
@@ -8,10 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/systems")
 public class SystemController {
 
     private SystemServiceImpl systemService;
@@ -21,41 +23,41 @@ public class SystemController {
         this.systemService = systemService;
     }
 
-    @GetMapping("/systems")
+    @GetMapping("/")
     public String findAll(Model model){
         List<System> systems = systemService.findAll();
         model.addAttribute("systems", systems);
         return "system-list";
     }
 
-    @GetMapping("/system-add")
+    @GetMapping("/add")
     public String createSystemForm(System system) {
         return "system-add";
     }
 
-    @PostMapping("/system-add")
+    @PostMapping("/add")
     public String createSystem(System system) {
         systemService.saveSystem(system);
-        return "redirect:/systems";
+        return "redirect:/systems/";
     }
 
-    @GetMapping("system-delete/{id}")
+    @GetMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") Integer id){
         systemService.deleteById(id);
-        return "redirect:/systems";
+        return "redirect:/systems/";
     }
 
-    @GetMapping("/system-edit/{id}")
+    @GetMapping("/edit/{id}")
     public String updateSystemForm(@PathVariable("id") Integer id, Model model){
         System system = systemService.findById(id);
         model.addAttribute("system", system);
         return "system-edit";
     }
 
-    @PostMapping("/system-edit")
+    @PostMapping("/edit")
     public String updateSystem(System system){
         systemService.saveSystem(system);
-        return "redirect:/systems";
+        return "redirect:/systems/";
     }
 
 }
